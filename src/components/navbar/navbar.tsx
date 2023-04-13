3c4de835e2be5f4aa8d9c33253c2aae937d20000
DenisPepper <thech@yandex.ru>
2023-04-14 01:23:11 +0300

    Add draft of navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import css from './navbar.module.css';
+
+export const Navbar = () => {
+    return (
+        <div className={css.navbar}>
+            <h2 className={css.title}>Cross-zero chat</h2>
+        </div>
+    );
+}
