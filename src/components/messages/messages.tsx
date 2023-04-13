dd0c3df5c26dc47570243e78ade6c7e958be2aa6
DenisPepper <thech@yandex.ru>
2023-04-14 01:22:33 +0300

    Add draft of messages list component

---
+++ src/components/messages/messages.tsx
@@ -0,0 +1,27 @@
+import css from './messages.module.css';
+import {Message} from "../message/message";
+
+export const Messages = () => {
+    return(
+        <div className={css.messages}>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+            <Message/>
+        </div>
+    );
+}
