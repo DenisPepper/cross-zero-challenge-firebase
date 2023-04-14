50ae419dcca871cc5710b2ca98d635dbac091d89
DenisPepper <thech@yandex.ru>
2023-04-14 20:33:34 +0300

    Modified styles for messages and sidebar

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,9 +1,34 @@
 import css from './message.module.css';
 
-export const Message = () => {
+interface MessageProps {
+    isOwner: boolean;
+    text: string;
+}
+
+export const Message = (props: MessageProps) => {
+    const {isOwner, text} = props;
+
     return (
-        <div>
-            Message
+        <div className={`${css.message} ${isOwner && css.owner}`}>
+            <div className={css.info}>
+
+                <img className={css.user_image}
+                     src='https://avatars.mds.yandex.net/i?id=2a00000179fc5a1c48815b62f752de84bdd0-3560974-images-thumbs&n=13&exp=1'
+                     alt='user image'/>
+
+                <span className={css.time}>11:05:00</span>
+            </div>
+
+            <div className={css.content}>
+                <p className={css.text}>
+                    {text}
+                </p>
+                <img
+                    src='https://avatars.mds.yandex.net/i?id=f171dd00b106ceb2cce8cc6192d52418_sr-5297754-images-thumbs&n=13'
+                    alt='message content image'
+                />
+            </div>
         </div>
     );
 }
+
--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -2,26 +2,24 @@ import css from './messages.module.css';
 import {Message} from "../message/message";
 
 export const Messages = () => {
-    return(
+    return (
         <div className={css.messages}>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
+            <Message
+                isOwner={false}
+                text={'Hello!'}
+            />
+            <Message
+                text={'Hello, this is very long long text'}
+                isOwner
+            />
+            <Message
+                text={'Hello, this is very long long text, but it is more longer, then ather messages and a little bit more'}
+                isOwner={false}
+            />
+            <Message
+                text={'Hello, this is very long long text'}
+                isOwner
+            />
         </div>
     );
 }
