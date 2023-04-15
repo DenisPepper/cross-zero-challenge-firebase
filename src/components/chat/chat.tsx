3b1dcb1b22e098827447bfa2f19f811360d34ff0
DenisPepper <thech@yandex.ru>
2023-04-15 14:32:37 +0300

    Add logout business logic

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,8 +1,23 @@
 import css from './chat.module.css';
 import {Messages} from "../messages/messages";
 import {Input} from "../input/input";
+import {signOut} from 'firebase/auth'
+import {auth} from "../../firebase";
+import {useContext} from "react";
+import {AuthContext} from "../auth-provider/auth-provider";
 
 export const Chat = () => {
+    const authContext = useContext(AuthContext);
+
+    const handleLogoutClick = async () => {
+        try {
+            await signOut(auth);
+            authContext.user = null;
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
     return(
         <div className={css.chat}>
             <div className={css.header}>
@@ -10,7 +25,7 @@ export const Chat = () => {
                 <div className={css.user}>
                     <img className={css.avatar} src={'https://avatars.mds.yandex.net/i?id=ea668d40a1b5f4cef64b1acbeff36791_sr-5834815-images-thumbs&n=13'} alt='user avatar'/>
                     <span className={css.name}>User</span>
-                    <button className={css.logout} type='button'>Logout</button>
+                    <button className={css.logout} type='button' onClick={handleLogoutClick}>Logout</button>
                 </div>
             </div>
             <Messages/>
