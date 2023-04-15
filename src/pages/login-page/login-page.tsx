8742645720c3ffe91cea8137a1b16dba39807fec
DenisPepper <thech@yandex.ru>
2023-04-15 14:32:10 +0300

    Add login business logic

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import css from './login-page.module.css';
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../../firebase";
+import {Link, useNavigate} from "react-router-dom";
+
+const inputName: Record<'email' | 'pass', string> = {
+    email: 'email',
+    pass: 'password',
+};
 
 export const LoginPage = () => {
+    const navigate = useNavigate();
 
-    const handleFormSubmit = (form: FormData) => {
-        console.log('submit')
+    const handleFormSubmit = async (form: FormData) => {
+        const email = form.get(inputName.email)?.toString() ?? '';
+        const password = form.get(inputName.pass)?.toString() ?? '';
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate('/');
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     return (
@@ -16,13 +32,13 @@ export const LoginPage = () => {
                     autoComplete='off'
                     onSubmit={(evt) => {
                         evt.preventDefault();
-                        handleFormSubmit(new FormData(evt.currentTarget));
+                        handleFormSubmit(new FormData(evt.currentTarget)).then();
                     }}>
-                    <input name='email-input' type="email" placeholder='email' autoComplete='username'/>
-                    <input name='password' type="password" placeholder='password' autoComplete='new-password'/>
+                    <input name={inputName.email} type="email" placeholder='email' autoComplete='username'/>
+                    <input name={inputName.pass} type="password" placeholder='password' autoComplete='current-password'/>
                     <button type='submit'>Sing in</button>
                 </form>
-                <p className={css.link}>You don`t have an account? <a href={'/login'}>Register</a></p>
+                <p className={css.link}>You don`t have an account? <Link to={'/register'}>Register</Link></p>
             </div>
         </section>
     );
